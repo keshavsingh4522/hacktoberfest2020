7276a9c8f7bf92f6e4735dc5be4752724e40c882
rimmon1234 <42698980+rimmon1234@users.noreply.github.com>
2024-10-01 21:59:54 +0530

    Add a Backup File Program

---
+++ Python/file_backup.py
@@ -0,0 +1,27 @@
+import os
+import shutil
+import datetime
+import schedule
+import time
+
+source_dir = r'C:\Users\Rimmon Bhowmick\OneDrive\Desktop\Python File'
+destination_dir = r'C:\Users\Rimmon Bhowmick\OneDrive\Desktop\Backup'
+
+def copy_folder_to_directory(source, dest):
+    today = datetime.date.today()
+    dest_dir = os.path.join(dest,str(today))
+
+    try:
+        shutil.copytree(source, dest_dir)
+        print(f'Folder Copied To: {dest_dir}')
+    except FileExistsError:
+        print(f'Folder Already exists in: {dest}')
+    except Exception as e:
+        print(f'Error copying folder: {e}')
+
+
+schedule.every().day.at("10:30").do(lambda: copy_folder_to_directory, source_dir, destination_dir)
+
+while True:
+    schedule.run_pending()
+    time.sleep(60)
